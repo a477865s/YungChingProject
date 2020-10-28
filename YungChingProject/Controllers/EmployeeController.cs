9f4d96bae0026fa493566b894270723ae582ebd1
a477865s@gmail.com <a477865s@gmail.com>
2020-10-28 16:01:12 +0800

    Create and Delate

--- YungChingProject/Controllers/EmployeeController.cs
+++ YungChingProject/Controllers/EmployeeController.cs
@@ -17,5 +17,33 @@ namespace YungChingProject.Controllers
             var prj = db.tEmployees.ToList();
             return View(prj);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tEmployee t)
+        {
+            db.tEmployees.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Home");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                int DeleteId = Convert.ToInt32(id);
+                tEmployee x = db.tEmployees.FirstOrDefault(p => p.fEmployeeId == DeleteId);
+                if (x != null)
+                {
+                    db.tEmployees.Remove(x);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Home");
+        }
+
+
+
     }
 }
